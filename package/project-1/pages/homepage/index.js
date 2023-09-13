406e89ab73ce820bc6fe0680bae55cc846d758a3
Prashant Kumar <kshant2017@gmail.com>
2023-09-13 17:20:10 +0530

    fixes

--- package/project-1/pages/homepage/index.js
+++ package/project-1/pages/homepage/index.js
@@ -1,10 +1,10 @@
-export { default } from '../../page-component/Homepage';
+// export { default } from '../../page-component/Homepage';
 
-// import React from 'react';
+import React from 'react';
 
-// function Dashboard() {
-// 	console.log('i am here');
-// 	return <div>Analytics</div>;
-// }
+function HomePage() {
+	console.log('i am here');
+	return <div>Analytics</div>;
+}
 
-// export default Dashboard;
+export default HomePage;
