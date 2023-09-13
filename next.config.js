987c875c2cff184d71d5143422067309f824ed86
Prashant Kumar <kshant2017@gmail.com>
2023-09-13 16:09:10 +0530

    fixes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,27 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {}
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    compiler : {
+      styledComponents : true,
+    },
+    webpack(config) {
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ["@svgr/webpack"]
+      });
+  
+      config.resolve.fallback = {...config.resolve.fallback,fs: false};
+  
+      return config;
+    }
+    
+  }
+  
+  module.exports = nextConfig
+  
